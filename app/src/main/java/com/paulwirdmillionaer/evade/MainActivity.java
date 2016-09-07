e06cd3face50eda71981aab24e5bbcfa15061ae8
Markus <fritzsma@gmail.com>
2016-09-07 13:50:53 +0200

    numero uno

--- app/src/main/java/com/paulwirdmillionaer/evade/MainActivity.java
+++ app/src/main/java/com/paulwirdmillionaer/evade/MainActivity.java
@@ -10,5 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+//ich bin numero uno nicht numero dos
 }
